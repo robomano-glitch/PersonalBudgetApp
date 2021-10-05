29ba89e6bfd770067d34c923ffdf6e88903f6cf2
Robert Sageme <rsageme@gmail.com>
2021-10-05 21:31:57 +0200

    Project Start
    -- Start of Project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,105 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="4">
+            <p class="fade">Robert Sageme</p>
+          </ion-col>
+          <ion-col size="4"> </ion-col>
+          <ion-col size="4">
+            <p class="fade">05 May, 2021</p>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      
+      <ion-grid>
+        <ion-row class="ion-no-margin ion-no-padding">
+          <ion-col size="12" class="ion-no-margin">
+            <ion-card class="card-1 ion-no-margin">
+              <ion-card-content>
+                <h1>5</h1>
+                <p>Members in this group</p>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+
+        <ion-row class="ion-no-margin ion-no-padding">
+          <ion-col size="6" class="ion-no-margin">
+            <ion-card class="card-2 ion-no-margin ion-no-padding">
+              <ion-card-content class="ion-no-padding">
+              <ion-row>
+                <ion-col size="8">
+                  
+                </ion-col>
+                <ion-col size="4">
+                  
+                </ion-col>
+              </ion-row>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+          
+          <ion-col size="6" class="ion-no-margin">
+            <ion-card class="card-3 ion-no-margin ion-no-padding">
+              <ion-card-content>
+                <h1>5</h1>
+                <p>Members in this group</p>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCardContent,
+  IonCard,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import {
+  star,
+  statsChartOutline,
+  personAddOutline,
+  printOutline,
+  locationOutline,
+  homeOutline,
+  calendarClearOutline,
+} from "ionicons/icons";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
-    IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar
-  }
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCardContent,
+    IonCard,
+  },
+  setup() {
+    return {
+      star,
+      statsChartOutline,
+      personAddOutline,
+      printOutline,
+      locationOutline,
+      homeOutline,
+      calendarClearOutline,
+    };
+  },
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
